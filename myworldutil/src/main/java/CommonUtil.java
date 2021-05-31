07df105b87a7823c99275a4f517cdd8409308d37
walle <walle.li@intellif.com>
2021-05-31 15:42:15 +0800

    测试cherry pick1

---
+++ myworldutil/src/main/java/CommonUtil.java
@@ -0,0 +1,153 @@
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.*;
+import java.security.MessageDigest;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
+
+
+@Slf4j
+public class CommonUtil {
+    /**
+     * 计算文件hash值
+     */
+    public static String hashFile(File file) throws Exception {
+        FileInputStream fis = null;
+        String sha256 = null;
+        try {
+            fis = new FileInputStream(file);
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte buffer[] = new byte[1024];
+            int length = -1;
+            while ((length = fis.read(buffer, 0, 1024)) != -1) {
+                md.update(buffer, 0, length);
+            }
+            byte[] digest = md.digest();
+            sha256 = byte2hexLower(digest);
+        } catch (Exception e) {
+            throw new Exception("计算文件hash值错误");
+        } finally {
+            try {
+                if (fis != null) {
+                    fis.close();
+                }
+            } catch (IOException e) {
+                log.error("文件hash关闭流异常，内存泄漏",e);
+            }
+        }
+        return sha256;
+    }
+
+    /**
+     * 计算文件hash值
+     */
+    public static String hashFile(byte[] data) throws Exception {
+        String sha256 = null;
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(data);
+            byte[] digest = md.digest();
+            sha256 = byte2hexLower(digest);
+        } catch (Exception e) {
+            throw new Exception("计算文件hash值错误");
+        }
+        return sha256;
+    }
+
+    private static String byte2hexLower(byte[] b) {
+        String hs = "";
+        String stmp = "";
+        for (int i = 0; i < b.length; i++) {
+            stmp = Integer.toHexString(b[i] & 0XFF);
+            if (stmp.length() == 1) {
+                hs = hs + "0" + stmp;
+            } else {
+                hs = hs + stmp;
+            }
+        }
+        return hs;
+    }
+
+
+    /**
+     * 使用gzip压缩字符串
+     * @param str 要压缩的字符串
+     * @return
+     */
+    public static String compress(String str) {
+        if (str == null || str.length() == 0) {
+            return str;
+        }
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        GZIPOutputStream gzip = null;
+        try {
+            gzip = new GZIPOutputStream(out);
+            gzip.write(str.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (gzip != null) {
+                try {
+                    gzip.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        String res = new sun.misc.BASE64Encoder().encode(out.toByteArray());
+        //gzip压缩带来的/命名问题，导致文件存储路径出现问题，用@符号替代/符号，解析需要进行反替代
+        return res.replaceAll("/","@");
+
+    }
+
+
+    /**
+     * 使用gzip解压缩
+     * @param compressedStr 压缩字符串
+     * @return
+     */
+    public static String uncompress(String compressedStr) {
+        if (compressedStr == null) {
+            return null;
+        }
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        ByteArrayInputStream in = null;
+        GZIPInputStream ginzip = null;
+        byte[] compressed = null;
+        String decompressed = null;
+        try {
+            compressed = new sun.misc.BASE64Decoder().decodeBuffer(compressedStr);
+            in = new ByteArrayInputStream(compressed);
+            ginzip = new GZIPInputStream(in);
+            byte[] buffer = new byte[1024];
+            int offset = -1;
+            while ((offset = ginzip.read(buffer)) != -1) {
+                out.write(buffer, 0, offset);
+            }
+            decompressed = out.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (ginzip != null) {
+                try {
+                    ginzip.close();
+                } catch (IOException e) {
+                }
+            }
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                }
+            }
+            if (out != null) {
+                try {
+                    out.close();
+                } catch (IOException e) {
+                }
+            }
+        }
+        return decompressed.replaceAll("@","/");
+    }
+}
